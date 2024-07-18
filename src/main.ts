05b69eb74078592af721d5026d51cc0f35360f91
wx_orange <wx_orange@orange007.com>
2024-07-18 10:02:58 +0800

    嵌入tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from '@/store';
 import router from '@/router';
+
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import '@/styles/index.scss';
+
+import 'tailwindcss/tailwind.css';
 
 const app = createApp(App);
 
